bcc5ee997229c440c0704392c7921d9013ecb31c
pussal <34545262+pussal@users.noreply.github.com>
2022-03-05 10:09:32 -0600

    Add files via upload

---
+++ CucumberProject/src/test/java/stepDefinitions/RunCuke.java
@@ -0,0 +1,16 @@
+package stepDefinitions;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="src/test/resources/features", glue= {"stepDefinitions"},
+monochrome=true,
+plugin= {"pretty", "html:target/HtmlReport/Reports"}
+		)
+
+public class RunCuke {
+
+}
